ec1ba38f455e9ca8d527c1cc98447a6964092901
Evan <evanlaw@hawaii.edu>
2015-07-24 12:21:57 -1000

    Modified password checker function

--- src/buyit.java
+++ src/buyit.java
@@ -9,6 +9,14 @@
 import java.util.*;
 
 public class buyit {
+	static boolean match;
+	static boolean Uppercase;
+	static boolean Lowercase;
+	static boolean min_length;
+	static boolean special;
+	static boolean conditions;
+	static boolean pwd_condition = false;
+	
 	private static final Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
 
@@ -34,10 +42,10 @@ public class buyit {
 		while (userInput != 4){
 			//Message to User
 			System.out.println("What would you like to do? ");
-			System.out.println("1 - User Registration \n");
-			System.out.println("2 - User Login \n");
-			System.out.println("3 - Search ADs \n");
-			System.out.println("4 - Exit \n");
+			System.out.println("1 - User Registration");
+			System.out.println("2 - User Login");
+			System.out.println("3 - Search ADs");
+			System.out.println("4 - Exit");
 			
 			//Catch numerical input for the var. userInput
 			userInput = input.nextInt(); 
@@ -50,25 +58,28 @@ public class buyit {
 				System.out.println("Username desired:\n");  //Ask for username desired
 				uid = input.nextLine();
 				
-				System.out.println("Password:\n"); //ask for password for user account
-				System.out.println("For account security, BuyIy accounts are required to meet the following requirements:");
-				System.out.println("1. Password must contain at least one UPPERCASE and LOWERCASE character.");
-				System.out.println("2. Password must have at least one special character. eg. !,@,#,$,%,&");
-				System.out.println("3. Password must be at least 12 characters long.\n");
-				pwd = input.nextLine();
+				while (pwd_condition == false)
+				{
+					System.out.println("Password:"); //ask for password for user account
+					System.out.println("For account security, BuyIy accounts are required to meet the following requirements:");
+					System.out.println("1. Password must contain at least one UPPERCASE and LOWERCASE character.");
+					System.out.println("2. Password must have at least one special character. eg. !,@,#,$,%,&");
+					System.out.println("3. Password must be at least 12 characters long.\n");
+					pwd = input.nextLine();
+					
+					System.out.println("Retype Password entered:\n"); //ask for password for user account (for comparison)
+					pwd2 = input.nextLine();
+					
+					//Call password function
+					CheckPassword(pwd, pwd2);
+				}
 				
-				System.out.println("Retype Password entered:\n"); //ask for password for user account (for comparison)
-				pwd2 = input.nextLine();
-				
-				//Call password function
-				CheckPassword(pwd, pwd2);
-				
-				System.out.println("First Name:\n");
+				/*System.out.println("First Name:\n");
 				fname = input.nextLine();
 				
 				System.out.println("Last Name:\n");
 				lname = input.nextLine();
-				/*
+				
 				System.out.println("Address:\n");
 				addr = input.nextLine();
 				
@@ -86,12 +97,13 @@ public class buyit {
 				
 				System.out.println("Credit Card No.:\n");
 				cc_num = input.nextInt();
-				*/
 				
-				//Print out all information onto the screen
+				//Send data over to the db
+				
+				//Print out user information onto the screen
 				System.out.println("Your username is: " + uid + " and your password is: " + pwd);
 				System.out.println("Congratulations " + fname + " " + lname +" ! Welcome to BuyIt!");
-				System.out.println("Please login to your BuyIt account to use site features!");
+				System.out.println("Your account has been created. Please login to your BuyIt account to use site features!");*/
 			}
 			else if (userInput == 2) //User login
 			{
@@ -99,11 +111,11 @@ public class buyit {
 				
 				//User enter credentials
 				//String uid;
-				System.out.println("Enter Username:\n");
+				System.out.println("Enter Username:");
 				uid = input.nextLine();
 				
 				//String pwd;
-				System.out.println("Enter Password:\n");
+				System.out.println("Enter Password:");
 				pwd = input.nextLine();
 				
 				// Look up db for exact credentials
@@ -132,24 +144,27 @@ public class buyit {
 	 * This function checks if the password matches and 
 	 * if the password matches the conditions for user accounts.
 	 */
-	public static void CheckPassword(String a, String b)
+	public static boolean CheckPassword(String a, String b)
 	{
-		boolean match = a.equals(b);					 //Check if passwords match
-		boolean Uppercase = !a.equals(a.toLowerCase());  //Check if password has uppercase
-		boolean Lowercase = !a.equals(a.toUpperCase());  //Check if password has lowercase
-		boolean min_length = a.length() >= 12;			 //Check if password is at least 12 or more
-		boolean special = !a.matches("[A-za-z0-9 ]*");	 //Check if password has special characters
-		boolean conditions = !(a.contains("AND") || a.contains("NOT")); 
+		match = a.equals(b);					 //Check if passwords match
+		Uppercase = !a.equals(a.toLowerCase());  //Check if password has uppercase
+		Lowercase = !a.equals(a.toUpperCase());  //Check if password has lowercase
+		min_length = a.length() >= 12;			 //Check if password is at least 12 or more
+		special = !a.matches("[A-za-z0-9 ]*");	 //Check if password has special characters
 		
 		if (!match) 
 			{
+				//Print warning. Return back to get password again
 				System.out.println("Warning! The passwords entered don't match!\n"); 
-				return;
+				return pwd_condition = false;
 			}
-		if (!Uppercase) System.out.println("Password must contain an uppercase!\n");
-		if (!Lowercase) System.out.println("Password must contain an lowercase!\n");
-		if (!min_length) System.out.println("Password must be at least 12 characters long!\n");
-		if (!special) System.out.println("Password must contain a special character!\n");
-		
+		else 
+		{
+			if (!Uppercase) System.out.println("Password must contain an uppercase!\n");
+			if (!Lowercase) System.out.println("Password must contain an lowercase!\n");
+			if (!min_length) System.out.println("Password must be at least 12 characters long!\n");
+			if (!special) System.out.println("Password must contain a special character!\n");
+			return pwd_condition = true;
+		}
 	}
 }
